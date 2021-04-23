dac7edb4a11ff1fab04ff9b558f2123c6250bfac
Marco Méndez <marco.mendez.ing@gmail.com>
2021-04-22 20:24:42 -0500

    Implemented the basics of the phantom troupe member, it appears on screen on a correct position and it gets destroyed

--- GameScene/GameScene.gd
+++ GameScene/GameScene.gd
@@ -6,6 +6,8 @@ const SECOND_CHANGE_BARRIER = 35
 const THIRD_CHANGE_BARRIER = 80
 const FOURTH_CHANGE_BARRIER = 150
 
+const PHANTOM_TROUPE_MEMBER_SCENE = preload("res://GameScene/characters_scenes/PhantomTroupeMember.tscn")
+
 # pT= phantom troupe 
 onready var pT_member_chance_to_appear = 0.4
 onready var enemy_chance_to_appear = 0.3
@@ -14,7 +16,7 @@ onready var friend_chance_to_appear = 0.3
 onready var timer = $Timer
 onready var elapsed_time = 0
 onready var elapsed_seconds = 0
-
+onready var first_phantom_troupe_member = false
 onready var random = RandomNumberGenerator.new()
 
 func _ready():
@@ -53,35 +55,32 @@ func calculate_elapsed_time(delta):
 	elapsed_seconds = fmod(elapsed_time,60)
 	
 
+# Creates a phantom troupe member
+func make_a_phantom_troupe_member_appear():
+	var phantom_troupe_member = PHANTOM_TROUPE_MEMBER_SCENE.instance()
+	add_child(phantom_troupe_member)
+
+
 # When the timer times out
 func _on_Timer_timeout():
 	random.randomize()
 	var random_number = random.randf_range(0,1)
 	
-	print("Elapsed seconds")
-	print(elapsed_seconds)
-	print("Deciding")
+	#print("Elapsed seconds")
+	#print(elapsed_seconds)
+	#print("Deciding")
 	# Deciding who has to appear in the game
 	if random_number <= pT_member_chance_to_appear:
-		print("make a phantom troupe member appear")
-		pass
+		make_a_phantom_troupe_member_appear()
 	if random_number <= enemy_chance_to_appear:
-		print("make an enemy appear")
+		#print("make an enemy appear")
 		pass
 	if random_number <= friend_chance_to_appear:
-		print("make a friend appear")
+		#print("make a friend appear")
 		pass
 	
 	timer.set_wait_time(CHAR_APPEARANCE_WAIT_TIME)
 	timer.start()
-
-
-
-
-
-
-
-
 
 
 
---
+++ GameScene/characters_scenes/PhantomTroupeMember.gd
@@ -0,0 +1,67 @@
+extends KinematicBody2D
+
+const CHROLLO_ID = 1
+const FEITAN_ID = 2
+const UVOGIN_ID = 3
+const PHINKS_ID = 4
+const SHALNARK_ID = 5
+const BONOLENOV_ID = 6
+const NOBUNAGA_ID = 7
+const FRANKLIN_ID = 8
+const MACHI_ID = 9
+const SHIZUKU_ID = 10
+const PAKUNODA_ID = 11
+const KALLUTO_ID = 12
+const KORTOPI_ID = 13
+
+const X_POS_MIN = 40
+const X_POS_MAX = 488
+const Y_POS_MIN = 72
+const Y_POS_MAX = 936
+
+onready var phantom_troupe_member_id = 0
+onready var random = RandomNumberGenerator.new()
+onready var sprite = $Sprite
+
+func _ready():
+	random.randomize()
+	phantom_troupe_member_id = random.randi_range(CHROLLO_ID,KORTOPI_ID)
+	match phantom_troupe_member_id:
+		CHROLLO_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/chrollo_head.png")
+		FEITAN_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/feitan_head.png")
+		UVOGIN_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/uvogin_head.png")
+		PHINKS_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/phinks_head.png")
+		SHALNARK_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/shalnark_head.png")
+		BONOLENOV_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/bonolenov_head.png")
+		NOBUNAGA_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/nobunaga_head.png")
+		FRANKLIN_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/franklin_head.png")
+		MACHI_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/machi_head.png")
+		SHIZUKU_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/shizuku_head.png")
+		PAKUNODA_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/pakunoda_head.png")
+		KALLUTO_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/kalluto_head.png")
+		KORTOPI_ID:
+			sprite.texture = load("res://GameScene/characters_scenes/kortopi_head.png")
+	
+	random.randomize()
+	position = Vector2(random.randi_range(X_POS_MIN,X_POS_MAX),random.randi_range(Y_POS_MIN,Y_POS_MAX))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Hurtbox_area_entered(area):
+	print("Muerto")
+	queue_free()
