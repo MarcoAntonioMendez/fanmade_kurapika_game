cdee7cd0f04ab99d2e91e4cfec1ec061014cc158
Marco Méndez <marco.mendez.ing@gmail.com>
2021-05-02 22:46:37 -0500

    The user's max score is now showed in the MenuScene.

--- MenuScreen/MenuScene.gd
+++ MenuScreen/MenuScene.gd
@@ -8,11 +8,16 @@ const SHRINKING = 2;
 onready var menuScreenBackgroundMusic = $AudioStreamPlayer
 onready var fadeInAnimation = $AnimationPlayer
 onready var backgroundVideo = $VideoPlayer
+onready var maxScoreLabel = $MaxScoreLabel
 
 var currentStartButtonFontSize;
 var startButtonState;
 
 func _ready():
+	#Reading user's max score
+	var score = read_score_file()
+	maxScoreLabel.text = maxScoreLabel.text + " " + str(score)
+	
 	currentStartButtonFontSize = MIN_START_BUTTON_FONT_SIZE;
 	startButtonState = GROWING;
 	fadeInAnimation.play("FadeIn")
@@ -42,3 +47,11 @@ func _process(delta):
 	
 	if backgroundVideo.is_playing() == false:
 		backgroundVideo.play()
+
+# Reads the content of the score file and returns it
+func read_score_file():
+	var file = File.new()
+	file.open("res://GameScene/user_score_file.dat", File.READ)
+	var content = file.get_as_text()
+	file.close()
+	return str(content)
