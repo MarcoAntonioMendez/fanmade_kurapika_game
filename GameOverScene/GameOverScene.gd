14923cbcb0888e4ebf547352a1098b1d4d3bc20c
Marco Méndez <marco.mendez.ing@gmail.com>
2021-05-08 15:15:14 -0500

    Implemnted fadeOut animation in GameOverScene

--- GameOverScene/GameOverScene.gd
+++ GameOverScene/GameOverScene.gd
@@ -5,6 +5,7 @@ const FALSE = "false"
 
 onready var score_text_label = $ScoreTextLabel
 onready var score_label = $ScoreLabel
+onready var fadeOutAnimation = $AnimationPlayer
 onready var was_max_score_update_file_path = "res://GameScene/was_max_score_updated.dat"
 onready var last_score_gotten_path = "res://GameScene/last_score.dat"
 
@@ -19,6 +20,9 @@ func _ready():
 	# Reading actual score user got.
 	var last_score = read_custom_file(last_score_gotten_path)
 	score_label.text = last_score
+	
+	# Playing fadeout animation
+	fadeOutAnimation.play("FadeOut")
 
 # Reads the content of a file, its path is provided as a parameter
 func read_custom_file(file_path):
